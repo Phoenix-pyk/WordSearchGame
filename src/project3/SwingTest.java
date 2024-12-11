abd124ed8f56f4d1fe936f1e3d9c8b79a8929714
Phoenix <161513433+Phoenix-pyk@users.noreply.github.com>
2024-12-10 23:36:25 -0500

    Delete src/project3/SwingTest.java

--- src/project3/SwingTest.java
+++
@@ -1,9 +0,0 @@
-package project3;
-
-import javax.swing.JOptionPane;
-
-public class SwingTest {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Swing is working!");
-    }
-}
